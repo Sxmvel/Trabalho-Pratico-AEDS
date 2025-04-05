d60579689d461a85e67d729f718c4c1f1aa99a92
Samuel Resende <samuelres444@gmail.com>
2025-04-05 14:38:33 -0300

    Iniciando o trabalho pratico

---
+++ src/trabalho_pratico_aeds/Trabalho_Pratico_AEDS.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package trabalho_pratico_aeds;
+
+/**
+ *
+ * @author Samuel
+ */
+public class Trabalho_Pratico_AEDS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
