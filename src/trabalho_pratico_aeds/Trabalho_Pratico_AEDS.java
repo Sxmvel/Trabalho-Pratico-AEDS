8d4024c19243074151f6a3f009c2c0a5a49d7cfb
Samuel Resende <samuelres444@gmail.com>
2025-04-06 17:02:36 -0300

    Algortimo de ordenacao counting sort concluido

--- src/trabalho_pratico_aeds/Trabalho_Pratico_AEDS.java
+++ src/trabalho_pratico_aeds/Trabalho_Pratico_AEDS.java
@@ -1,10 +1,11 @@
 package trabalho_pratico_aeds;
 
-import java.util.*;
+import java.util.Random;
+
 
 public class Trabalho_Pratico_AEDS {
 
-    public static void countingSort(int[] Array) {
+    public static void countingSortCrescente(int[] Array) {
         int maior_Chave = 0;
 
         for (int i = 1; i < Array.length; i++) {
@@ -43,41 +44,92 @@ public class Trabalho_Pratico_AEDS {
         }
     }
 
-    public static void ordenacao_Crescente_CountingSort() {
-        int[] tamanho_Vetores = {100, 500, 1000};
-
-        System.out.print("Vetores Desordenados: ");
-        for (int i = 0; i < tamanho_Vetores.length; i++) {
-            int[] vetores_Preencheer = new int[tamanho_Vetores[i]];
-            Random seedFixa = new Random(12345L);
-
-            for (int j = 0; j < vetores_Preencheer.length; j++) {
-                vetores_Preencheer[j] = (int) seedFixa.nextLong(100);
-            }
-
-            System.out.println("");
-            for (int j = 0; j < vetores_Preencheer.length; j++) {
-                System.out.print(vetores_Preencheer[j] + " ");
+    public static void countingSortDecrescente(int[] arr) {
+        // Encontra o maior chave
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
             }
         }
-        System.out.println("");
-        System.out.print("Vetores Ordenados: ");
-        for (int i = 0; i < tamanho_Vetores.length; i++) {
-            int[] vetores_Preencheer = new int[tamanho_Vetores[i]];
-            Random seedFixa = new Random(12345L);
 
-            for (int j = 0; j < vetores_Preencheer.length; j++) {
-                vetores_Preencheer[j] = (int) seedFixa.nextLong(100);
-            }
-            System.out.println("");
-            for (int j = 0; j < vetores_Preencheer.length; j++) {
-                countingSort(vetores_Preencheer);
-                System.out.print(vetores_Preencheer[j] + " ");
-            }
+        // Cria array de contagem
+        int[] count = new int[max + 1];
+
+        // Conta a frequência de cada elemento
+        for (int i = 0; i < arr.length; i++) {
+            count[arr[i]]++;
+        }
+
+        // Acumula os valores da direita para a esquerda
+        for (int i = count.length - 2; i >= 0; i--) {
+            count[i] += count[i + 1];
+        }
+
+        // Cria array de saída
+        int[] sorted = new int[arr.length];
+
+        // Preenche o array de saída (de trás para frente para manter estabilidade)
+        for (int i = arr.length - 1; i >= 0; i--) {
+            int num = arr[i];
+            sorted[count[num] - 1] = num;
+            count[num]--;
+        }
+
+        // Copia o resultado para o array original
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sorted[i];
         }
     }
 
+    public static void ordenacao_CountingSort() {
+        int[] tamanho_Vetores = {10, 20, 30};
+
+        System.out.print("Gerando vetores de forma crescente usando o couting sort: ");
+        for (int i = 0; i < tamanho_Vetores.length; i++) {
+            int[] vetores_Preencheer = new int[tamanho_Vetores[i]];
+            Random seedFixa = new Random(12345L);
+
+            for (int j = 0; j < vetores_Preencheer.length; j++) {
+                vetores_Preencheer[j] = (int) seedFixa.nextLong(100);
+            }
+            System.out.println("");
+            for (int j = 0; j < vetores_Preencheer.length; j++) {
+                countingSortCrescente(vetores_Preencheer);
+            }
+            System.out.print("Vetor de tamanho: " + tamanho_Vetores[i] + " gerado e ordenado crescente!");
+        }
+
+        System.out.println("\n");
+        System.out.println("Gerando vetores de forma Decrescente usando o couting sort: ");
+        for (int i = 0; i < tamanho_Vetores.length; i++) {
+            int[] vetores_Preencheer = new int[tamanho_Vetores[i]];
+            Random seedFixa = new Random(12345L);
+            for (int j = 0; j < vetores_Preencheer.length; j++) {
+                vetores_Preencheer[j] = (int) seedFixa.nextLong(100);
+            }
+            for (int j = 0; j < vetores_Preencheer.length; j++) {
+                countingSortDecrescente(vetores_Preencheer);
+            }
+            System.out.print("Vetor de tamanho: " + tamanho_Vetores[i] + " gerado e ordenado Decrescente!\n");
+        }
+        
+        System.out.println("");
+        System.out.println("Gerando Vetores aleatorios usando o couting sort:");
+        for (int i = 0; i < tamanho_Vetores.length; i++) {
+            int[] vetores_Preencheer = new int[tamanho_Vetores[i]];
+            Random seedFixa = new Random(12345L);
+
+            for (int j = 0; j < vetores_Preencheer.length; j++) {
+                vetores_Preencheer[j] = (int) seedFixa.nextLong(100);
+            }
+            System.out.println("Vetor de tamanho: " + tamanho_Vetores[i] + " gerado aleatorio!");
+        }
+
+    }
+
     public static void main(String[] args) {
-        ordenacao_Crescente_CountingSort();
+        ordenacao_CountingSort();
+       
     }
 }
